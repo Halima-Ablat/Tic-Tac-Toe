2eabb30de8b8b0a3525e9e5e305425d5ff619348
Halima <152661929+Halima-Ablat@users.noreply.github.com>
2025-01-29 21:13:30 +1100

    Add files via upload

--- TicTacToe.jsx
+++ TicTacToe.jsx
@@ -1,23 +1,19 @@
 import React, { useState } from "react";
 
 function TicTacToe() {
+  const [squares, setSquares] = useState(Array(9).fill(""));
+  const [isXTurn, setIsXTurn] = useState(true);
 
-  const[squares, setSquares] = useState(Array(9).fill(''));
-  console.log(squares)
   return (
     <div className="container">
       <div className="game-container">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
+        {squares.map((square, index) => (
+          <div key={index} className="square">
+            {square}
+          </div>
+        ))}
       </div>
-      <p>Next player is </p>
+      <p>Next player is</p>
       <button>Restart</button>
     </div>
   );
